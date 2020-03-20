360bef0e791e937c670dd7613060da64b82f1176
PiyushDatta <piyushdattaca@gmail.com>
2020-03-20 03:48:48 -0400

    added some code to test out the json, works well, everything is good now

--- src/main.cc
+++ src/main.cc
@@ -5,21 +5,19 @@ int main(int argc, char const *argv[]) {
   ExampleClient example_client;
   std::pair<CURLcode, std::string> curl_resp = example_client.GETRequest();
 
-  using json = nlohmann::json;
-  // std::string m_json_data = nlohmann::json::parse(curl_resp.second).get<std::string>();
-  // std::cout << "\nSuccessfully parsed JSON data" << '\n';
-  // std::cout << "\nJSON data size: " << m_json_data.size() << '\n';
-  // std::cout << "\nJSON data received:" << '\n';
-  // std::cout << m_json_data << '\n';
   // got a good json parsable response
-  // try {
-
-  // } catch (const json::exception &e) {
-  //   std::cout << "ERROR: Could not parse HTTP data as JSON" << '\n';
-  //   // output exception information
-  //   std::cout << "ERROR: " << e.what() << '\n';
-  //   std::cout << "Exception id: " << e.id << '\n';
-  // }
+  try {
+    nlohmann::json m_json_data = nlohmann::json::parse(curl_resp.second);
+    std::cout << "\nSuccessfully parsed JSON data" << '\n';
+    std::cout << "\nJSON data size: " << m_json_data.size() << '\n';
+    std::cout << "\nJSON data received:" << '\n';
+    std::cout << m_json_data << '\n';
+  } catch (const nlohmann::json::exception &e) {
+    std::cout << "ERROR: Could not parse HTTP data as JSON" << '\n';
+    // output exception information
+    std::cout << "ERROR: " << e.what() << '\n';
+    std::cout << "Exception id: " << e.id << '\n';
+  }
 
   return 0;
 }
