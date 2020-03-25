8d0f2ec2a2930b1d4b1af3ffd4c3c9b952e70e8e
PiyushDatta <piyushdattaca@gmail.com>
2020-03-25 04:16:16 -0400

    take out the hello world test

--- src/main.cc
+++ src/main.cc
@@ -2,7 +2,6 @@
 #include "utils/include/util_functions.h"
 
 int main(int argc, char const *argv[]) {
-  util_func::debug_log("hello world", true);
   ServerSocket sock_server;
   sock_server.keepAcceptingAndListening();
   return 0;
